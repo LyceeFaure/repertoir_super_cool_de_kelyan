51ac264febc38e2e98d6924ebfa04a188ce7724d
Keylan BEKHOUH <kbekhouh@education.local>
2024-09-16 14:25:54 +0200

    next patient pls

--- arachez.py
+++ arachez.py
@@ -1,1 +1,2 @@
 je vais vous raconter l'histoire de Baptiste il est super mechant et il utilise ses ecouteurs en cours
+vamos habla en espana porque hay Baptiste que es un human loco porque no quiere apprender una nueva lengua
