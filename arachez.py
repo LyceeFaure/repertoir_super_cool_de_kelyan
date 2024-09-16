de1676763700f81b0c700efdae131efb01c2eb60
Keylan BEKHOUH <kbekhouh@education.local>
2024-09-16 14:50:34 +0200

    python

--- arachez.py
+++ arachez.py
@@ -1,3 +1,2 @@
-je vais vous raconter l'histoire de Baptiste il est super mechant et il utilise ses ecouteurs en cours
-vamos habla en espana porque hay Baptiste que es un human loco porque no quiere apprender una nueva lengua
-aracher vous les gars soyer dans cela
+def ia():
+	print("on aime pas les ias")
